44eff5a2eb137d048ec30a2487db1a748f295a17
CodeBulletin <CodeBulletin@github.com>
2022-08-03 22:08:29 +0530

    +About Me(WIP)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,18 @@
-const { src, dest, watch, series, parallel } = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const purgecss = require("gulp-purgecss");
-const ts = require("gulp-typescript");
-const tsConfig = ts.createProject("tsconfig.json")
 
 function buildStyles() {
     return src('src/sass/**/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(purgecss({ content: ['*.html'] }))
+        .pipe(purgecss({ content: ['*.html'], safelist:['open', 'active-icon'] }))
         .pipe(dest('lib/css'))
 }
 
 function fDevStyles() {
     return src('src/sass/**/*.scss')
         .pipe(sass())
-        .pipe(purgecss({ content: ['*.html'] }))
+        .pipe(purgecss({ content: ['*.html'], safelist:['open', 'active-icon']  }))
         .pipe(dest('lib/css'))
 }
 
@@ -36,16 +34,6 @@ function watchfDStyle() {
     watch(['src/sass/**/*.scss'], DevStyles)  
 }
 
-function buildScript() {
-    return tsConfig.src().pipe(tsConfig()).js.pipe(dest('lib/js'))
-}
-
-function watchScript() {
-    watch(['src/ts/*.ts'], buildScript)
-}
-
-const ts_build = series(buildScript, watchScript);
-
-exports.build = parallel(series(buildStyles, watchStyle), ts_build)
-exports.fdev = parallel(series(fDevStyles, watchfStyle), ts_build)
-exports.dev = parallel(series(DevStyles, watchfDStyle), ts_build)
+exports.build = series(buildStyles, watchStyle)
+exports.fdev = series(fDevStyles, watchfStyle)
+exports.dev = series(DevStyles, watchfDStyle)
